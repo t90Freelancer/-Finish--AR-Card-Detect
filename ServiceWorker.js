f142c9b3e6ecfaa262b21b51e52e2de50736071b
t90Freelancer <t90freelancer@gmail.com>
2024-03-28 00:22:48 +0700

    Finish

---
+++ ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "DefaultCompany-[AR] Card Detect-0.1";
+const contentToCache = [
+    "Build/[Finish] Ar Card Detect.loader.js",
+    "Build/[Finish] Ar Card Detect.framework.js",
+    "Build/[Finish] Ar Card Detect.data",
+    "Build/[Finish] Ar Card Detect.wasm",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
